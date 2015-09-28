5eba80b9b864436b50f703e22a4c317e2e1529d1
crr <crr@us.ibm.com>
2015-09-28 16:20:39 -0400

    Set up demo code

---
+++ src/main/java/com/urbancode/demo/HelloWorld.java
@@ -0,0 +1,9 @@
+package com.urbancode.demo;
+
+import com.urbancode.dependency.HelloWorldPrinter;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        HelloWorldPrinter.print();
+    }
+}
