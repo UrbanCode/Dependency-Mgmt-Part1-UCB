4ef9caa17cc4385fb511d88ddb631c8e0e0e6a93
crr <crr@us.ibm.com>
2016-01-27 14:00:43 -0500

    Revert "Modified to incorporate a simple JUnit test"
    This reverts commit 510b97f716df239017e81816912755f73bf8cc8c.

--- src/main/java/com/urbancode/demo/HelloWorld.java
+++ src/main/java/com/urbancode/demo/HelloWorld.java
@@ -4,6 +4,6 @@ import com.urbancode.dependency.HelloWorldPrinter;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println(HelloWorldPrinter.print());
+        HelloWorldPrinter.print();
     }
 }
